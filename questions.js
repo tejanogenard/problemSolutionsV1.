f039c754e27787a1029a9c4df0d92dcd602fedc1
Genard Ivan Tejano <genardivanteajno@Genards-MBP.lan>
2022-12-16 21:32:34 -0800

    added leetcode problem 242

--- questions.js
+++ questions.js
@@ -69,3 +69,32 @@ var getConcatenation = function(nums) {
 
         return array3 = nums.concat(double);
 };
+
+
+/*
+    242. Valid Anagram
+
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+    typically using all the original letters exactly once.
+
+    Example 1:
+
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+    Example 2:
+
+    Input: s = "rat", t = "car"
+    Output: false
+
+    Sooo.. how do we do this... 
+    we'll flip the array and loop through it checking if it matches the original.
+
+
+    
+
+
+
+
+
+*/
